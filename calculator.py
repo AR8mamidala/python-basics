d52cf723e983bc7fff900ccd0603badc6983844b
Rishitha Mamidala <rishithamamidala999@gmail.com>
2026-09-12 15:13:12 +0100

    Create calculator v1.py

---
+++ calculator.py
@@ -0,0 +1,43 @@
+# Function for addition
+def add(num1,num2):
+  sum=num1+num2
+  return sum
+
+# Function for subtraction
+def subtract(num1,num2):
+  subtracted_value=num1-num2
+  return subtracted_value
+
+#Function for multiplication
+def multiply(num1,num2):
+  multiplied_value=num1*num2
+  return multiplied_value
+
+#Function for division
+def divide(num1,num2):
+  divided_value=num1/num2
+  return divided_value
+
+num1=print("Choose a number.")
+num2=print("Choose another number.")
+
+print("1.Add")
+print("2.Subtract")
+print("3.Multiply")
+print("4.Divide")
+print("5.Exit")
+option = int(print("Select an option from above:"))
+
+if option=1 then:
+subtract(num1,num2)
+elif option=2 then:
+subtract(num1,num2)
+elif option=3 then:
+multiply(num1,num2)
+elif option=4 then:
+divide(num1,num2)
+elif option=5 then:
+print("Bye bye!")
+else:
+print("Choose a valid option number.")
+
